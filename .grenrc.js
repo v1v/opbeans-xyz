fe7611159a2c55387032c2eec93fd84d64440234
Victor Martinez <victormartinezrubio@gmail.com>
2019-09-18 11:52:02 +0100

    chore: remove bug from the text (#6)

--- .grenrc.js
+++ .grenrc.js
@@ -5,7 +5,7 @@ module.exports = {
     "ignoreCommitsWith": ["chore", "refactor", "style"],
     "ignoreIssuesWith": ["no-release"],
     "ignoreTagsWith": ["-rc", "-alpha", "-beta", "test", "current"],
-    "ignoreLabels": ["closed", "automation", "enhancement", "fix",
+    "ignoreLabels": ["closed", "automation", "enhancement", "fix", "bug",
       "internal", "feature", "feat", "docs", "chore", "refactor", "ci",
       "perf", "test", "style"],
     "groupBy": {
